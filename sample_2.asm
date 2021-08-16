ef099d823c03d4adbc507f216469793939f45003
Jose Cancinos <jmcancinos@mi.unc.edu.ar>
2021-08-16 16:19:14 -0300

    JSON instruction extended set completed

---
+++ sample_2.asm
@@ -0,0 +1,15 @@
+ADDI $0, $17,  4
+ADDI $zero, $s1,  4
+ADDI $zero, $s2, 3
+ADDI $zero, $s3, 1
+ADDI $zero, $s4, 2
+
+BEQ $s3, $s4, else
+ADD $s1, $s2, $s0
+J end
+else:
+SUB $s1, $s2, $s0
+end:
+
+ADD $zero, $zero, $zero// not necessary
+HALT
